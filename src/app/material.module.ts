18297ca69596449913277c8959d4068b962afacd
Héctor Alvarado <hectoralexanderalvarado@gmail.com>
2023-02-09 11:59:16 -0600

    Mejoras en el diseño El formulario funciona correctamente

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import { MatOptionModule } from '@angular/material/core';
-import { MatSelectModule} from '@angular/material/select'
+import { MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { MatSelectModule} from '@angular/material/select'
     MatButtonModule,
     MatFormFieldModule,
     MatOptionModule,
-    MatSelectModule
+    MatSelectModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
--- src/app/renta/renta.component.ts
+++ src/app/renta/renta.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+/*enum departamento {
+  'Ventas',
+  'Contabilidad',
+  'Administración',
+  'TI'
+};*/
+
 @Component({
   selector: 'app-renta',
   templateUrl: './renta.component.html',
@@ -9,16 +16,29 @@ export class RentaComponent implements OnInit {
   
   
   //Variables
-  primerApellido : string = '';
-  segundoApellido : string = '';
-  nombre : string = '';
-  departamento : string = '';
-  salario:number = 0;
+  primerApellido : string = 'Primer Apellido';
+  segundoApellido : string = 'Segundo Apellido';
+  nombre : string = 'Nombre';
+  //departamento: departamento;
+  departamento: string[] = ['Ventas','Contabilidad','Administración','TI'];
+  departamentoSeleccionado : string = 'Ventas';
+  /*departamento:typeof departamento = departamento {
+    Ventas: 'ventas'
+    Contabilidad: 'Contabilidad'
+    Administracion:'Administración'
+    TI : 'TI'
+  };*/
 
-
+  salario : number = 0;
+  renta : number = 0;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calcularRenta () {
+    this.renta = this.salario - 10;
   }
 
 }
